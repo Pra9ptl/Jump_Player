a72089623843b513667c9e2fc879caf4346cae83
Pranav Patel <patelpranav1313@gmail.com>
2019-06-11 09:33:55 -0400

    SKAction added to poop to run forever

--- jump/GameScene.swift
+++ jump/GameScene.swift
@@ -21,6 +21,21 @@ class GameScene: SKScene {
         self.robot = self.childNode(withName: "robot")
         self.poop = self.childNode(withName: "poop")
         self.ground = self.childNode(withName: "ground")
+        
+        // mkae the poop move back and forth
+        
+        //------------------------------------
+        
+        // 1. mkae your SKAction
+        
+        let m1 = SKAction.moveTo(x: -500, duration: 2)
+        let m2 = SKAction.moveTo(x: 400, duration: 2)
+        
+        // 2. put sction into a sequence
+        let sequence = SKAction.sequence([m1,m2])
+        
+        // 3. apply sequence to sprite
+        self.poop?.run(SKAction.repeatForever(sequence))
     }
     
     
@@ -28,8 +43,11 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         //to apply the impulse we need a physics body
-        let jumpAction = SKAction.applyImpulse(CGVector(dx: 0, dy: 4000), duration: 0.5)
-        
+        let jumpAction = SKAction.applyImpulse(CGVector(dx: 0, dy: 3500), duration: 0.2)
+        //jump is control in 3 ways
+        // 1. cahnge the mass
+        // 2. Change size (height/width)
+        // 3. Change force that is applied (CGVector)
         self.robot!.run(jumpAction)
         
     }
