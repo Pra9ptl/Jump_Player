8ff8d52e72a2029304976d8edaab0c22993fe2ab
Pranav Patel <patelpranav1313@gmail.com>
2019-06-11 08:31:40 -0400

    initial commit

---
+++ jump/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  jump
+//
+//  Created by MacStudent on 2019-06-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
